29c67b93b07ce61be57baac772162763e5837de4
Son Dajung <ekwjd02@naver.com>
2018-06-25 04:54:54 +0000

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,28 @@
+class HomeController < ApplicationController
+    def index
+        # render 'home/index'
+        # erb :index
+    end
+
+    def lotto
+        # 로또번호 6개 추천하는 ruby 코드
+        @lotto = (1..45).to_a.sample(6) # 1~45의 숫자를 .a(arr)배열로 랜덤(sample)하게 6개(6) 뽑는다
+        # render 'home/lotto'
+    end
+    
+    def lunch
+        # 점심메뉴 추천 ruby 코드
+        # 1. 원하는 메뉴를 menu 배열에 저장
+        # 2. menu에서 랜덤으로 하나뽑아 @lunch에 넣어주고
+        # 3 'home/lunch'에서 @lunch를 보여준다.
+        menu = ["20층", "김까", "시래기"]
+        @lunch = menu.sample
+        # render 'home/lunch'
+    end
+    
+    def search
+       # form을 통해 검색어를 입력 받아
+       # naver, daum, google에 넘겨준다.
+       
+    end
+end
